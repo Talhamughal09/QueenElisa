683ad37d53b6e0c97f694c7dad8fc9caef48ee70
Talha Mughal <164769621+Talhamughal09@users.noreply.github.com>
2024-10-07 14:45:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: 'QUEENELISA;;;==AOQ1mSE1EeUpUY5lTTjR3ROdjRulVMyNWTHJWT4oUSNVmcL5mSfpEMs9VRjolcLZUarlkQpwd=855378190329',
+  SESSION_ID: 'QUEENELISA;;;==wdXhVcXtGUNZWTy9EdHpGUs9VUQ1WRw82TuVWaMpkcL1GdYxmWyV2VURGajM1NRVVRkNmVpwd=339042172329',
   ALIVE_REACT: '🖤',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
